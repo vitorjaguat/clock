4585147ea2ead33f7bb079a2bcc5792c84557c01
Vitor Jaguat <vitorbutkus@gmail.com>
2023-01-16 18:42:23 -0300

    a circle divided in 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Clock from './Clock';
+import Clock from './clock-mtc/Clockmtc';
 
 function App() {
   return (
---
+++ src/clock-mtc/Clockmtc.jsx
@@ -0,0 +1,44 @@
+import './Clockmtc.css';
+
+export default function Clock() {
+  return (
+    <ul class="circle">
+      <li>
+        <div class="text">心</div>
+      </li>
+      <li>
+        <div class="text">小肠</div>
+      </li>
+      <li>
+        <div class="text">膀胱</div>
+      </li>
+      <li>
+        <div class="text">肾</div>
+      </li>
+      <li>
+        <div class="text">心包</div>
+      </li>
+      <li>
+        <div class="text">三焦</div>
+      </li>
+      <li>
+        <div class="text">胆</div>
+      </li>
+      <li>
+        <div class="text">肝</div>
+      </li>
+      <li>
+        <div class="text">肺</div>
+      </li>
+      <li>
+        <div class="text">大肠</div>
+      </li>
+      <li>
+        <div class="text">胃</div>
+      </li>
+      <li>
+        <div class="text">脾</div>
+      </li>
+    </ul>
+  );
+}
